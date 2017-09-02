35778445cd054f8e4582c0ad65bfdd270a541f3c
silambarasanmkce <silambarasansilambarasan007@gmail.com>
2017-09-02 11:08:19 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+
+int number;
+
+scanf("%d",number);
+
+if(number==0)
+
+printf("zero");
+
+else if(number>0)
+
+printf("positive");
+
+else
+
+printf("negative");
+
+return 0;
+
+}
