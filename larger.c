e983006d9aa2e9e771556bdff3aaeec96fcc0fa7
silambarasanmkce <silambarasansilambarasan007@gmail.com>
2017-09-02 12:15:20 +0530

    Add files via upload

---
+++ larger.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+
+int number1,number3,number2;
+
+scanf("%d %d %d",number1,number2,number3);
+if((number1>number2)&&(number1>number3))
+printf("number1 is larger");
+else if(number2>number3)
+printf("number2 is larger");
+else
+printf("number3 is larger");
+return 0;
+}
